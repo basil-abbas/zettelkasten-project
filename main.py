f942cfbee0f131924aee6fd4d1e59985ee9a4612
Basil <basil.abbas.live@gmail.com>
2026-03-09 08:13:01 -0500

    Re uploading same stuff

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import modules.source_notes as source_notes_module
 
 
-# test_url = https://www.youtube.com/watch?v=g2-_pnmhO4A&list=PLyxTU7oQdPUUaTxPYdGe7x_V03x2SI5Zf&index=3
+# https://www.youtube.com/watch?v=g2-_pnmhO4A&list=PLyxTU7oQdPUUaTxPYdGe7x_V03x2SI5Zf&index=3
 
 
 print("Welcome to the Zettelkasten Note Taking App\nPlease enter some form of source notes (URL, Video, etc.)")
